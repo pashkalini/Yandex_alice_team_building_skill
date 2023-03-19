6115ba38b01143fc0f07e59bbfd3a176cc7fcb8d
pashkalini <pashkalini2000@gmail.com>
2023-03-19 20:02:57 +0300

    Изменены тексты активностей, игр, тем для разговора, а также расписаны команды Алисы, если сказать "Помощь". Реализованы следующие функции: 1. включение фоновой музыки на 10 и 20 минут 2. текстовые перебивки каждые 2 минуты фоновой музыки и ее смена

--- index.py
+++ index.py
@@ -12,6 +12,32 @@ def make_response(text, tts=None):
     }
 
 
+# def repeat_message():
+#     with open("text_to_repeat.txt", 'r', encoding='utf8') as f:
+#         msg = f.read()
+#     return msg
+
+# def save_msg_text(msg):
+#     with open("text_to_repeat.txt", 'w', encoding='utf8') as f:
+#         f.write(msg)
+#         return
+
+# тексты для перебивок музыки через каждые 2 минуты
+text_18_min = "Первые две минуты прошли. Вечеринка в самом начале, продолжаем набирать обороты!"
+text_16_min = "Еще две минутки закончились. Продолжайте, у вас еще 16 минут."
+text_14_min = "Так так так, осталось 14 минут, Вперед-вперед!"
+text_12_min = "Ку-ку, прошло еще две минут. У вас в запасе еще 12!"
+text_10_min = "Очередные две минуты прошли! Остается - 10, дерзайте!"
+text_8_min = "Прошло две минуты! Надеюсь, вы уже начали знакомиться. Если хотите перейти к следующей активности,\
+скажите \"Дальше\" или можете спросить меня: «‎Что ты умеешь?». А пока продолжаем, у вас еще 8 минут."
+text_6_min = "Прошло еще две минуты! Осталось 6 минут."
+text_4_min = "Больше половины отведенного времени прошло! Если хотите перейти к следующей активности,\
+скажите \"Дальше\" или можете спросить меня: «‎Что ты умеешь?». У вас еще 4 минуты."
+text_2_min = "Такс, ну вот еще две минутки прошли. Пошла последняя двухминуточка, успевайте!"
+text_fin_1 = "О, оу! Время вышло! Если хотите еще каких-нибудь активностей, можете спросить меня: «‎Что ты умеешь?»"
+text_fin_2 = "Ну всё, время вышло! Хорошо, что познакомились! Если хотите продолжить, можете спросить меня: «‎Что ты умеешь?»"
+
+
 # приветствует пользователя
 def welcome_message(event):
     text = "Сегодня были пары? Кажется, я стала прогульщицей...\
@@ -25,7 +51,7 @@ def welcome_message(event):
 def fallback(event):
     response_1 = "Ой, а что вы сказали? Не расслышала."
     response_2 = "До меня сегодня долго доходит. Что вы хотите сделать: познакомиться или повеселиться?"
-    response_3 = "Ничего в голову не приходит. Надо записаться на курсы по развитию фантазии. Простите, что подвела."
+    response_3 = "Ничего в голову не приходит. Надо записаться на курсы по развитию фантазии. Чтобы узнать, какие команды у меня есть скажите - \"Помощь\""
     responses = [response_1, response_2, response_3]
     random_response = random.choice(responses)
     return make_response(random_response)
@@ -34,7 +60,11 @@ def fallback(event):
 # Выбирает одну из активностей для знакомства
 def start_meeting_activity(event):
     bg = bg_music()
-    bg_tts = bg[1]
+    bg_tts_1 = bg[1]
+    bg_tts_2 = bg[2]
+    bg_tts_3 = bg[3]
+    bg_tts_4 = bg[4]
+    bg_tts_5 = bg[5]
     bg_text = bg[0]
     with open("activity_1.txt", 'r', encoding='utf8') as text_1, \
             open("activity_2.txt", 'r', encoding='utf8') as text_2, \
@@ -42,13 +72,23 @@ def start_meeting_activity(event):
         activities = [text_1, text_2, text_3]
         random_text = random.choice(activities)
         activity_text = random_text.read() + bg_text
-        return make_response(activity_text, activity_text + bg_tts)
+        return make_response(activity_text, activity_text + bg_tts_1 + text_8_min + \
+                             bg_tts_2 + text_6_min + bg_tts_3 + text_4_min + bg_tts_4 + text_2_min + bg_tts_5 + text_fin_2)
 
 
 # выбирает одну из игр
 def start_game(event):
     bg = bg_music()
-    bg_tts = bg[1]
+    bg_tts_1 = bg[1]
+    bg_tts_2 = bg[2]
+    bg_tts_3 = bg[3]
+    bg_tts_4 = bg[4]
+    bg_tts_5 = bg[5]
+    bg_tts_6 = bg[6]
+    bg_tts_7 = bg[7]
+    bg_tts_8 = bg[8]
+    bg_tts_9 = bg[9]
+    bg_tts_10 = bg[10]
     bg_text = bg[0]
     with open("game_1.txt", 'r', encoding='utf8') as text_1, \
             open("game_2.txt", 'r', encoding='utf8') as text_2, \
@@ -56,16 +96,19 @@ def start_game(event):
         games = [text_1, text_2, text_3]
         random_text = random.choice(games)
         game_text = random_text.read() + bg_text
-        return make_response(game_text, game_text + bg_tts)
+        return make_response(game_text, game_text + bg_tts_1 + text_18_min + bg_tts_2 + text_16_min + \
+                             bg_tts_3 + text_14_min + bg_tts_4 + text_12_min + bg_tts_5 + \
+                             text_10_min + bg_tts_6 + text_8_min + bg_tts_7 + text_6_min + bg_tts_8 + text_4_min + \
+                             bg_tts_9 + text_2_min + bg_tts_10 + text_fin_1)
 
 
 # что ты умеешь
 def what_can_you_do(event):
-    text = "Я знаю, как вас познакомить или избавить от скуки. Включусь в вашу компанию, поболтаем, поиграем. \
-            А ещё могу  предложить тему для разговора.\
-            Хотите познакомиться или сыграть в игру?"
+    text = "Я знаю, как вас познакомить или избавить от скуки. Включусь в вашу компанию, поболтаем, поиграем.\
+            \nА ещё могу  предложить тему для разговора или включить песню, которую вы точно знаете.\
+            \nХотите познакомиться или сыграть в игру?"
     tts = "Я знаю, как вас познакомить или избавить от скуки. Включусь в вашу компанию, поболтаем, поиграем. \
-            А ещё могу  предложить тему для разговора. sil <[200]>\
+            А ещё могу  предложить тему для разговора или включить песню, которую вы точно знаете. sil <[200]>\
             Хотите познакомиться или сыграть в игру?"
     return make_response(text, tts)
 
@@ -76,7 +119,7 @@ def help(event):
         return make_response(commands.read())
 
 
-#  выбирает рандомно игру или активность
+# выбирает рандомно игру или активность
 def game_vs_activity(event):
     game = start_game(event)
     activity = start_meeting_activity(event)
@@ -86,21 +129,29 @@ def game_vs_activity(event):
 
 # предлагает тему для разговора
 def theme_variants(event):
-    text = "Предлагаю поболтать на такую тему: "
+    text = "Предлагаю каждому по очереди ответить на такой вопрос: "
     bg = bg_music()
-    bg_tts = bg[1]
+    bg_tts_1 = bg[1]
+    bg_tts_2 = bg[2]
+    bg_tts_3 = bg[3]
+    bg_tts_4 = bg[4]
+    bg_tts_5 = bg[5]
     bg_text = bg[0]
-    themes = ["На какие кружки вас водили родители в детстве?",
-              "Неловкие ситуации.",
-              "Что бы вы не стали делать, даже за миллион долларов?",
-              "Кто каким спортом занимается?",
-              "Мистические истории.",
-              "Животные.",
-              "Что вы смотрите на YouTube?",
-              "Любимый язык программирования.",
-              "Любите путешествовать? Куда ездили последний раз?"]
-    theme_text = text + random.choice(themes) + bg_text
-    return make_response(theme_text, theme_text + bg_tts)
+    text_timer = "\n\nЯ засеку 10 минут, чтобы вы могли поболтать."
+    themes = ["На какие кружки вас водили родители в детстве?", \
+              "Какие неловкие ситуации у вас были?", \
+              "Что бы вы не стали делать, даже за миллион долларов?", \
+              "Кто каким спортом занимается?", \
+              "Вы когда-нибудь сталкивались с чем-то мистическим?", \
+              "Как вы относитесь к животным?", \
+              "Что вы смотрите на YouTube?", \
+              "Какой ваш любимый язык программирования и почему?", \
+              "Любите путешествовать? Куда ездили последний раз?",
+              "Вы когда-нибудь сталкивались с чем-то мистическим?"]
+    theme_text = text + random.choice(themes) + text_timer + bg_text
+    return make_response(theme_text, \
+                         theme_text + bg_tts_1 + text_8_min + bg_tts_2 + \
+                         text_6_min + bg_tts_3 + text_4_min + bg_tts_4 + text_2_min + bg_tts_5 + text_fin_1)
 
 
 # обработка команд по типу "дальше", "давай другое"
@@ -115,31 +166,36 @@ def another_activity(event):
 # включает песню, которую знают все
 def play_music(event):
     text = "Включаю, эту песню вы точно знаете!"
-    tts_1 = "<speaker audio=\"dialogs-upload/9298f90d-da88-42dd-bafc-253e0505f2f6/2d3ac5af-13a5-4586-bba2" \
-            "-8522512d8f4a.opus\">"
-    tts_2 = "<speaker audio=\"dialogs-upload/9298f90d-da88-42dd-bafc-253e0505f2f6/9a8114c7-6428-4835-a9c2" \
-            "-8dd3ab41c490.opus\">"
-    tts_3 = "<speaker audio=\"dialogs-upload/9298f90d-da88-42dd-bafc-253e0505f2f6/8e6febcd-0972-4f67-a095" \
-            "-2f622350858e.opus\">"
-    tts_4 = "<speaker audio=\"dialogs-upload/9298f90d-da88-42dd-bafc-253e0505f2f6/6615efee-4ee3-4fb9-863b" \
-            "-35932ec3e5d0.opus\">"
-    tts_5 = "<speaker audio=\"dialogs-upload/9298f90d-da88-42dd-bafc-253e0505f2f6/2c6ce01b-7668-4130-9a84" \
-            "-991db17f6bcd.opus\">"
+    tts_1 = "<speaker audio=\"dialogs-upload/9298f90d-da88-42dd-bafc-253e0505f2f6/2d3ac5af-13a5-4586-bba2-8522512d8f4a.opus\">"
+    tts_2 = "<speaker audio=\"dialogs-upload/9298f90d-da88-42dd-bafc-253e0505f2f6/9a8114c7-6428-4835-a9c2-8dd3ab41c490.opus\">"
+    tts_3 = "<speaker audio=\"dialogs-upload/9298f90d-da88-42dd-bafc-253e0505f2f6/8e6febcd-0972-4f67-a095-2f622350858e.opus\">"
+    tts_4 = "<speaker audio=\"dialogs-upload/9298f90d-da88-42dd-bafc-253e0505f2f6/6615efee-4ee3-4fb9-863b-35932ec3e5d0.opus\">"
+    tts_5 = "<speaker audio=\"dialogs-upload/9298f90d-da88-42dd-bafc-253e0505f2f6/ffd20248-52d4-4070-83d0-651fdd18f666.opus\">"
     music_tts = [tts_1, tts_2, tts_3, tts_4, tts_5]
-    return make_response(text, text + random.choice(music_tts))
+    next_text = "Вот это я понимаю классика! Аж настроение поднялось. Надеюсь и вам песенка понравилась. \
+    Если хотите продолжить тусовку, спросите: «‎Что ты умеешь?»"
+    return make_response(text + "\n\n 🎶 воспроизводится музыка", text + random.choice(music_tts) + next_text)
 
 
 # фоновая музыка
 def bg_music():
+    # длительность трека - 2 минуты
     text = "\n\nА чтобы вы про меня не забыли, включу фоновую музыку."
-    tts_1 = "<speaker audio=\"dialogs-upload/9298f90d-da88-42dd-bafc-253e0505f2f6/7ea00568-2068-453f-949f" \
-            "-e03258f55f02.opus\">"
-    tts_2 = "<speaker audio=\"dialogs-upload/9298f90d-da88-42dd-bafc-253e0505f2f6/d851b591-671c-4534-8e53" \
-            "-3dea98483853.opus\">"
-    tts_3 = "<speaker audio=\"dialogs-upload/9298f90d-da88-42dd-bafc-253e0505f2f6/cf64664f-31f7-47d5-92e3" \
-            "-1e3b2cca9576.opus\">"
-    bg_tts = [tts_1, tts_2, tts_3]
-    bg = [text, random.choice(bg_tts)]
+    tts_1 = "<speaker audio=\"dialogs-upload/9298f90d-da88-42dd-bafc-253e0505f2f6/7ea00568-2068-453f-949f-e03258f55f02.opus\">"
+    tts_2 = "<speaker audio=\"dialogs-upload/9298f90d-da88-42dd-bafc-253e0505f2f6/5a1543b1-4c70-4c00-82da-b2102e307a15.opus\">"
+    tts_3 = "<speaker audio=\"dialogs-upload/9298f90d-da88-42dd-bafc-253e0505f2f6/8cceef7d-4ab5-4d32-993a-7ee117d15881.opus\">"
+    tts_4 = "<speaker audio=\"dialogs-upload/9298f90d-da88-42dd-bafc-253e0505f2f6/c0c15b91-4744-4561-b54b-8fce0fc5dffa.opus\">"
+    tts_5 = "<speaker audio=\"dialogs-upload/9298f90d-da88-42dd-bafc-253e0505f2f6/2260fe71-abf5-4a5c-9175-0f41751b903c.opus\">"
+    tts_6 = "<speaker audio=\"dialogs-upload/9298f90d-da88-42dd-bafc-253e0505f2f6/4aaf4efc-ac5a-4a74-a98b-2ab6e2db7ee4.opus\">"
+    tts_7 = "<speaker audio=\"dialogs-upload/9298f90d-da88-42dd-bafc-253e0505f2f6/9446b849-a407-4f78-ab7d-cc6fc9f84935.opus\">"
+    tts_8 = "<speaker audio=\"dialogs-upload/9298f90d-da88-42dd-bafc-253e0505f2f6/8eafbf56-cb54-4288-8646-6a79f3b27546.opus\">"
+    tts_9 = "<speaker audio=\"dialogs-upload/9298f90d-da88-42dd-bafc-253e0505f2f6/79db73ad-67b1-4377-a7b3-b843a649c067.opus\">"
+    bg_tts = [tts_1, tts_2, tts_3, tts_4, tts_5, tts_6, tts_7, tts_8, tts_9]
+    # выбираем 10 рандомных фоновых песен для чередования
+    bg = [text, random.choice(bg_tts), random.choice(bg_tts), random.choice(bg_tts), random.choice(bg_tts),
+          random.choice(bg_tts), \
+          random.choice(bg_tts), random.choice(bg_tts), random.choice(bg_tts), random.choice(bg_tts),
+          random.choice(bg_tts)]
     return bg
 
 
@@ -147,22 +203,34 @@ def bg_music():
 def handler(event, context):
     intents = event['request']['nlu']['intents']
     if event['session']['new']:
-        return welcome_message(event)
+        msg = welcome_message(event)
+        # save_msg_text(msg)
+        return msg
     elif 'start_meeting_activity' in intents:
-        return start_meeting_activity(event)
+        msg = start_meeting_activity(event)
+        return msg
     elif 'start_game' in intents:
-        return start_game(event)
+        msg = start_game(event)
+        return msg
     elif 'what_can_you_do' in intents:
-        return what_can_you_do(event)
+        msg = what_can_you_do(event)
+        return msg
     elif 'help' in intents:
-        return help(event)
+        msg = help(event)
+        return msg
     elif 'game_vs_activity' in intents:
-        return game_vs_activity(event)
+        msg = game_vs_activity(event)
+        return msg
     elif 'theme_variants' in intents:
-        return theme_variants(event)
+        msg = theme_variants(event)
+        return msg
     elif 'another_activity' in intents:
-        return another_activity(event)
+        msg = another_activity(event)
+        return msg
     elif 'play_music' in intents:
-        return play_music(event)
+        msg = play_music(event)
+        return msg
+    # elif 'repeat_message' in intents:
+    #     repeat_message
     else:
         return fallback(event)
